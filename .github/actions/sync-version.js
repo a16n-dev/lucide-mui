575281cd9f0b5d6f2c225940dfbeeee26de2f0cb
Alex <alexn400@gmail.com>
2023-07-30 10:13:04 +1200

    Add script to bump version to stay in line with lucide

---
+++ .github/actions/sync-version.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// Load the package.json file
+let packageJson = fs.readFileSync('package.json');
+let packageObj = JSON.parse(packageJson);
+
+// Specify your dependency
+let dependency = "lucide";
+
+// Get the version of the dependency
+let dependencyVersion = packageObj.dependencies[dependency];
+
+// Update the version of your package
+packageObj.version = dependencyVersion;
+
+// Write the updated package.json file back to disk
+fs.writeFileSync('package.json', JSON.stringify(packageObj, null, 2));
